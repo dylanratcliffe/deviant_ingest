45e3d0720ee05930904f9efb03958ea72d0b8676
Dylan Ratcliffe <dylanratcliffe@outlook.com>
2020-12-03 21:45:01 +0000

    Added ingestion

--- ingest/dgraph.go
+++ ingest/dgraph.go
@@ -2,8 +2,12 @@ package ingest
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"strings"
+	"time"
 
+	"github.com/dylanratcliffe/sdp/go/sdp"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/dgraph-io/dgo/v200"
@@ -71,6 +75,22 @@ GloballyUniqueName: string @index(hash) .
 Attributes: uid .
 Metadata: uid .
 LinkedItems: [uid] @reverse .
+
+type Metadata {
+	BackendName
+	RequestMethod
+	Timestamp
+	BackendDuration
+	BackendDurationPerItem
+	BackendPackage
+}
+
+BackendName: string @index(hash) .
+RequestMethod: string .
+Timestamp: dateTime @index(hour) .
+BackendDuration: int .
+BackendDurationPerItem: int .
+BackendPackage: string @index(hash) .
 `
 
 // SetupSchemas Will create the schemas required for ingest to work. This will
@@ -86,3 +106,186 @@ func SetupSchemas(dg *dgo.Dgraph) error {
 
 	return err
 }
+
+// Upsert Models an upsert action. Can be converted to JSON and executed
+// directly
+type Upsert struct {
+	Query     string      `json:"query,omitempty"`
+	Mutations []*Mutation `json:"mutations,omitempty"`
+}
+
+// Mutation represents an individual mutation to be made as part of an upsert.
+// It can optionally have its own condiition
+type Mutation struct {
+	Condition string        `json:"cond,omitempty"`
+	Set       []interface{} `json:"set,omitempty"`
+}
+
+// ItemNode Represents an item, it also is able to return a full list of
+// muatations
+type ItemNode struct {
+	Type            string       `json:"Type,omitempty"`
+	UniqueAttribute string       `json:"UniqueAttribute,omitempty"`
+	Context         string       `json:"Context,omitempty"`
+	Metadata        MetadataNode `json:"Metadata,omitempty"`
+	Attrributes     Attributes   `json:"-"`
+	// LinkedItems     []LinkedItem `json:"-"`
+	item *sdp.Item `json:"-"`
+}
+
+// Mutations Returns a list of mutations that can be
+func (i *ItemNode) Mutations() []*Mutation {
+	return []*Mutation{
+		// Create the item if it's not there
+		{
+			Condition: fmt.Sprintf("@if(eq(len(%v.item), 0))", i.item.GloballyUniqueName()),
+			Set: []interface{}{
+				i,
+			},
+		},
+		// TODO: Add a mutation where we update the linked items regardless
+
+		// Update the attributes & metadata. There is no condition here since we
+		// want to make sure this is up to date regardless
+		{
+			Set: []interface{}{
+				i.Attrributes,
+				i.Metadata,
+			},
+		},
+	}
+}
+
+// MarshalJSON Custom marshalling functionality that adds derived fields
+// required for DGraph
+func (i ItemNode) MarshalJSON() ([]byte, error) {
+	type Alias ItemNode
+	return json.Marshal(&struct {
+		UID                  string `json:"uid"`
+		DType                string `json:"dgraph.type,omitempty"`
+		UniqueAttributeValue string `json:"UniqueAttributeValue,omitempty"`
+		GloballyUniqueName   string `json:"GloballyUniqueName,omitempty"`
+		Alias
+	}{
+		UID:                  fmt.Sprintf("uid(%v.item)", i.item.GloballyUniqueName()),
+		DType:                "item",
+		UniqueAttributeValue: i.item.UniqueAttributeValue(),
+		GloballyUniqueName:   i.item.GloballyUniqueName(),
+		Alias:                (Alias)(i),
+	})
+}
+
+// UnmarshalJSON Converts from JSON to ItemNode
+func (i *ItemNode) UnmarshalJSON(value []byte) error {
+	return json.Unmarshal(value, i)
+}
+
+// Query returns a query that should match specifically this item. It will also
+// export the following variables:
+//
+//   * `{GloballyUniqueName}.item`: UID of this item
+//   * `{GloballyUniqueName}.attributes`: UID of this item's attributes
+//   * `{GloballyUniqueName}.metadata`: UID of this item's metadata
+func (i *ItemNode) Query() string {
+	var lines []string
+	var gun string
+
+	gun = i.item.GloballyUniqueName()
+
+	lines = make([]string, 5)
+	lines[0] = fmt.Sprintf("%v(func: eq(GloballyUniqueName, \"%v\")) {", gun, gun)
+	lines[1] = fmt.Sprintf("  %v.item as uid", gun)
+	lines[2] = fmt.Sprintf("  %v.attributes as Attributes", gun)
+	lines[3] = fmt.Sprintf("  %v.metadata as Metadata", gun)
+	lines[4] = "}"
+
+	return strings.Join(lines, "\n")
+}
+
+// MetadataNode Represents metadata as serialised for DGraph
+type MetadataNode struct {
+	BackendName            string        `json:"BackendName,omitempty"`
+	RequestMethod          string        `json:"RequestMethod,omitempty"`
+	Timestamp              time.Time     `json:"Timestamp,omitempty"`
+	BackendDuration        time.Duration `json:"BackendDuration,omitempty"`
+	BackendDurationPerItem time.Duration `json:"BackendDurationPerItem,omitempty"`
+	BackendPackage         string        `json:"BackendPackage,omitempty"`
+	item                   *sdp.Item
+}
+
+// MarshalJSON Custom marshalling functionality that adds derived fields
+// required for DGraph
+func (i MetadataNode) MarshalJSON() ([]byte, error) {
+	type Alias MetadataNode
+	return json.Marshal(&struct {
+		UID   string `json:"uid"`
+		DType string `json:"dgraph.type,omitempty"`
+		Alias
+	}{
+		UID:   fmt.Sprintf("uid(%v.metadata)", i.item.GloballyUniqueName()),
+		DType: "itemMetadata",
+		Alias: (Alias)(i),
+	})
+}
+
+// UnmarshalJSON Converts from JSON to MetadataNode
+func (i *MetadataNode) UnmarshalJSON(value []byte) error {
+	return json.Unmarshal(value, i)
+}
+
+// TODO: Custom marshalling for MetadataNode: uid and dgraph.type
+
+// Attributes Represents the attributes of a single item
+type Attributes struct {
+	Map  map[string]interface{}
+	item *sdp.Item
+}
+
+// MarshalJSON Custom marshalling functionality that adds derived fields
+// required for DGraph
+func (a Attributes) MarshalJSON() ([]byte, error) {
+	// We want to store the attributes in the format {type}.{attribute}. This is
+	// so that we don't have a clash of predicates. We wan this becuase we will
+	// also be doing dynamic type generation and I think it needs to be unique.
+	// though this might be worth looking into...
+
+	// Re-map the map
+	newMap := make(map[string]interface{})
+
+	for k, v := range a.Map {
+		newMap[fmt.Sprintf("%v.%v", a.item.GetType(), k)] = v
+	}
+
+	// Append the dgraph type and UID
+	newMap["dgraph.type"] = fmt.Sprintf("%vAttributes", a.item.GetType())
+	newMap["uid"] = fmt.Sprintf("uid(%v.attributes)", a.item.GloballyUniqueName())
+
+	return json.Marshal(newMap)
+}
+
+// UnmarshalJSON Converts from JSON to Attributes
+func (a *Attributes) UnmarshalJSON(value []byte) error {
+	dotMap := make(map[string]interface{})
+	a.Map = make(map[string]interface{})
+
+	err := json.Unmarshal(value, &dotMap)
+
+	if err != nil {
+		return err
+	}
+
+	// Remove the {type}.{attribute} formatting
+	for k, v := range dotMap {
+		// Split on each dot
+		sections := strings.Split(k, ".")
+
+		// Select the last element
+		a.Map[sections[len(sections)-1]] = v
+	}
+
+	return nil
+}
+
+// func ToUpsert(items []*sdp.Item) []map[string]interface{} {
+
+// }
--- ingest/handler.go
+++ ingest/handler.go
@@ -1,19 +1,51 @@
 package ingest
 
 import (
+	"context"
+	"encoding/json"
+	"time"
+
 	"github.com/dgraph-io/dgo/v200"
+	"github.com/dgraph-io/dgo/v200/protos/api"
 	"github.com/dylanratcliffe/sdp/go/sdp"
 	"github.com/nats-io/nats.go"
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 	"google.golang.org/protobuf/proto"
 )
 
 // NewUpsertHandler CReates a NATS message handler that upserts items into the given database
 func NewUpsertHandler(dgraph *dgo.Dgraph) func(*nats.Msg) {
 	return func(msg *nats.Msg) {
-		item := &sdp.Item{}
+		var item *sdp.Item
+		var itemNode ItemNode
+		var itemUpsert Upsert
+		var txn *dgo.Txn
+		var mu *api.Mutation
+		var res *api.Response
+		var err error
+		var b []byte
+		var ctx context.Context
+		var cancel context.CancelFunc
+		var timeout time.Duration
+		var errFields log.Fields
 
-		err := proto.Unmarshal(msg.Data, item)
+		// TODO: Ensure that this is reading from memory so it's fast
+		viper.SetDefault("databaseTimeout", "1s")
+		timeout, err = time.ParseDuration(viper.GetString("databaseTimeout"))
+
+		// Get the timeout
+		ctx, cancel = context.WithTimeout(
+			context.Background(),
+			timeout,
+		)
+		defer cancel()
+
+		// TODO: Add timing of these operations and log the timing
+
+		item = &sdp.Item{}
+
+		err = proto.Unmarshal(msg.Data, item)
 
 		if err != nil {
 			return
@@ -23,5 +55,97 @@ func NewUpsertHandler(dgraph *dgo.Dgraph) func(*nats.Msg) {
 			"type":                  item.GetType(),
 			"numLinkedItemRequests": len(item.GetLinkedItemRequests()),
 		}).Debug("Processing item")
+
+		// Convert to a local representation so that we can extract the database
+		// queries from it
+		itemNode = ItemNode{
+			Type:            item.GetType(),
+			UniqueAttribute: item.GetUniqueAttribute(),
+			Context:         item.GetContext(),
+			Metadata: MetadataNode{
+				BackendName:            item.GetMetadata().GetBackendName(),
+				RequestMethod:          item.GetMetadata().GetRequestMethod().String(),
+				Timestamp:              item.GetMetadata().GetTimestamp().AsTime(),
+				BackendPackage:         item.GetMetadata().GetBackendPackage(),
+				BackendDuration:        item.GetMetadata().GetBackendDuration().AsDuration(),
+				BackendDurationPerItem: item.GetMetadata().GetBackendDurationPerItem().AsDuration(),
+				item:                   item,
+			},
+			Attrributes: Attributes{
+				Map:  item.GetAttributes().GetAttrStruct().AsMap(),
+				item: item,
+			},
+			item: item,
+		}
+
+		// Store logs for later
+		errFields = log.Fields{
+			"itemGloballyUniqueName": item.GloballyUniqueName(),
+			"backendName":            itemNode.Metadata.BackendName,
+			"requestMethod":          itemNode.Metadata.RequestMethod,
+			"timestamp":              itemNode.Metadata.Timestamp,
+			"backendPackage":         itemNode.Metadata.BackendPackage,
+			"backendDuration":        itemNode.Metadata.BackendDuration,
+			"backendDurationPerItem": itemNode.Metadata.BackendDurationPerItem,
+			"attributes":             itemNode.Attrributes,
+		}
+
+		// Create a transaction
+		txn = dgraph.NewTxn()
+
+		// Defer cancel of the transaction. This means that if there is an
+		// unhandled exception it will cleanly cancel the transaction. If the
+		// transaction has already been committed then the discard won't do
+		// anything
+		defer txn.Discard(context.Background())
+
+		// Create the mutation
+		itemUpsert = Upsert{
+			Query:     itemNode.Query(),
+			Mutations: itemNode.Mutations(),
+		}
+
+		// Convert to JSON
+		b, err = json.Marshal(itemUpsert)
+
+		if err != nil {
+			errFields["err"] = err
+			log.WithFields(errFields).Error("Item JSON Marshal failed before database insertion")
+
+			return
+		}
+
+		mu = &api.Mutation{
+			SetJson: b,
+		}
+
+		// Execute the mutation
+		res, err = txn.Mutate(ctx, mu)
+
+		if err != nil {
+			errFields["err"] = err
+			log.WithFields(errFields).Error("Error during upsert of item into database")
+
+			return
+		}
+
+		log.WithFields(log.Fields{
+			"latency": res.GetLatency(),
+			"metrics": res.GetMetrics(),
+		}).Debug("Upsert complete")
+
+		// Commit the transaction
+		err = txn.Commit(ctx)
+
+		if err != nil {
+			errFields["err"] = err
+			log.WithFields(errFields).Error("Error during database transaction commit")
+
+			return
+		}
+
+		log.WithFields(log.Fields{
+			"itemGloballyUniqueName": item.GloballyUniqueName(),
+		}).Error("Commit complete")
 	}
 }
